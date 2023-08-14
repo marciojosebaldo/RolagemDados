03c1e480a85b2883da3b46a7a5b6200c11951a99
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-14 19:03:19 -0300

    Add files via upload

---
+++ src/main/java/RolagemDados/RolagemDados/RolagemDadosApplication.java
@@ -0,0 +1,13 @@
+package RolagemDados.RolagemDados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolagemDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolagemDadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RolagemDados/RolagemDados/RolagemDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package RolagemDados.RolagemDados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolagemDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
