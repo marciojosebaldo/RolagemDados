0b1908c0eb665d1b6a6ded39817879edc3d99318
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-14 19:52:48 -0300

    S

--- src/main/java/RolagemDados/RolagemDados/Service/S_RolagemDados.java
+++ src/main/java/RolagemDados/RolagemDados/Service/S_RolagemDados.java
@@ -2,11 +2,24 @@ package RolagemDados.RolagemDados.Service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Service
 public class S_RolagemDados {
 
     private int qtdeFaces;
     private int qtdeDados;
 
+    List listaSorteioDados = new ArrayList<>(qtdeDados);
 
+    Random gerador = new Random();
+
+    public int calculaValorDados(int qtdeFaces, int qtdeDados) {
+        for (int i = 0; i  <= qtdeDados; i++) {
+
+        }
+        return 0;
+    }
 }
