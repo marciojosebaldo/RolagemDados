cd4e76bfcfc4fdb96e8e8e6cfab61e4b79443bee
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-14 21:17:52 -0300

    Service

--- src/main/java/RolagemDados/RolagemDados/Service/S_RolagemDados.java
+++ src/main/java/RolagemDados/RolagemDados/Service/S_RolagemDados.java
@@ -3,7 +3,6 @@ package RolagemDados.RolagemDados.Service;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 @Service
@@ -11,20 +10,34 @@ public class S_RolagemDados {
 
     private int qtdeFaces;
     private int qtdeDados;
+    int somaDados = 0;
+    int maiorResultado = 0;
 
-    List listaSorteioDados = new ArrayList<>(qtdeDados);
+    ArrayList<Integer> listaSorteioDados = new ArrayList();
 
     Random gerador = new Random();
 
     public void calculaValorDados(int qtdeFaces, int qtdeDados) {
+        this.qtdeFaces = qtdeFaces;
+        this.qtdeDados = qtdeDados;
+
         for (int i = 0; i <= qtdeDados; i++) {
-            if (gerador.nextInt() < qtdeFaces) {
-                listaSorteioDados.add(gerador);
-            }
+            int resultado = gerador.nextInt(qtdeFaces);
+            listaSorteioDados.add(resultado);
         }
     }
 
     public void gerarListaSorteioDados() {
         listaSorteioDados.forEach(System.out::println);
+    }
+
+    public void somaDadosLista() {
+        for (Integer n : listaSorteioDados) {
+            somaDados += n;
+        }
+    }
+
+    public void maiorResultadoLista() {
+        maiorResultado = listaSorteioDados.stream().mapToInt(Integer::intValue).max().orElse(0);
     }
 }
